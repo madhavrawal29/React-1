3cb2535c76d8d663cda7ca21abcfcc30638f25ff
Madhav Rawal <112850999+madhavrawal29@users.noreply.github.com>
2024-03-17 22:22:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './Components/Header/Header.jsx';
+import CoreConcepts from './Components/CoreConcepts.jsx';
+import Examples from './Components/Examples.jsx';
+
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <CoreConcepts />
+        <Examples />
+      </main>
+    </div>
+  );
+}
+
+export default App;
